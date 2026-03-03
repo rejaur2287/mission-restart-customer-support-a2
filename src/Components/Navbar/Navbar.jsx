a2ffa1b53b1ce846bb4175f98bc5858082a6d48e
rejaur2287 <rezaurewubd@gmail.com>
2026-03-03 22:32:32 +0600

    Project setup and navbar section added.

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100 w-11/12 mx-auto px-0 py-3">
+        <div className="navbar-start w-3/4">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+              <li>
+                <a>Teams</a>
+              </li>
+              <li>
+                <a>Changelog</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Download</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <a className="font-bold text-2xl items-center">CS — Ticket System</a>
+        </div>
+        <div className="navbar-end flex place-content-end justify-end w-full mr-4">
+          <ul className="menu menu-horizontal px-1 hidden lg:flex text-lg">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+            <li>
+              <a>Teams</a>
+            </li>
+            <li>
+              <a>Changelog</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Download</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end w-1/4 items-center">
+          <a className="btn btn-primary font-bold text-lg">
+            <span className="text-xl mr-1">+</span> New Ticket
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
